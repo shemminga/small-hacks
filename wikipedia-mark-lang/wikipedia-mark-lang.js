339ce068e91b70cc67051e2bfb94e93e2232e39b
Sjoerd Hemminga <sjoerd@hemminga-online.nl>
2018-05-06 23:46:49 +0200

    Some tweaks (better alignment, don't highlight inside more dialog)

--- wikipedia-mark-lang/wikipedia-mark-lang.js
+++ wikipedia-mark-lang/wikipedia-mark-lang.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Wikipedia for Multilinguals
-// @version     0.2
+// @version     0.3
 // @homepageURL http://sjoerd-hemminga.com/2012/03/wikipedia-for-multilinguals/
 // @updateURL   https://raw.githubusercontent.com/shemminga/small-hacks/master/wikipedia-mark-lang/wikipedia-mark-lang.js
 // @downloadURL https://raw.githubusercontent.com/shemminga/small-hacks/master/wikipedia-mark-lang/wikipedia-mark-lang.js
@@ -11,8 +11,8 @@
 // ==/UserScript==
 
 (function() {
-	var css = 'li.interwiki-en, li.interwiki-nl, li.interwiki-simple { font-weight: bold; margin-left: -13px !important; } ' +
-        'li.interwiki-en:before, li.interwiki-nl:before, li.interwiki-simple:before { content:"⚫ "; }';
+	var css = '#p-lang li.interwiki-en, #p-lang li.interwiki-nl, #p-lang li.interwiki-simple { font-weight: bold; margin-left: -18px !important; display: list-item !important; } ' +
+        '#p-lang li.interwiki-en:before, #p-lang li.interwiki-nl:before, #p-lang li.interwiki-simple:before { content:"⚫ "; }';
 	var cssTextNode = document.createTextNode(css);
 	var style = document.createElement('style');
 	style.appendChild(cssTextNode);
